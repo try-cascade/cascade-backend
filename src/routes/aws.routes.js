9da5c8d329dc94418925c4a3ee1aa1b07ab3811b
Natalie Thompson <Natalie.A.Thompson@outlook.com>
2022-10-28 10:00:13 -0500

    Modified the create application, create env, create service, and get service routes.
    Co-authored-by: Anne Tiotuico <amtiotuico@gmail.com>
    Co-authored-by: Rona Hsu <ronahsu@gmail.com>
    Co-authored-by: Yueun Kim <yueun.owl@gmail.com>

--- src/routes/aws.routes.js
+++ src/routes/aws.routes.js
@@ -3,13 +3,16 @@ const router = express.Router();
 const awsController = require('../controllers/aws.controllers')
 
 // Get listing of clusters
-router.get('/clusters', awsController.clusters)
+// router.get('/clusters', awsController.clusters)
 
 router.post('/bucket', awsController.createBucket)
 
 router.post('/environment', awsController.addEnvironmentToBucket)
 
-router.get('/:app/environment/:env', awsController.environment)
+// router.get('/:app/environment/:env', awsController.environment)
 
+router.post('/service', awsController.addServiceToBucket)
+
+router.get('/:app/environment/:env/services', awsController.services)
 
 module.exports = router
